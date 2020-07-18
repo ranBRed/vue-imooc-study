608c7088fe7f1da5f18d5d30a95db7fa6595829f
liliran <1341495943@qq.com>
2020-07-18 09:50:20 +0800

    second-commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <keep-alive exclude="Detail">
+<!--      与Detail进行匹配的组件都不会缓存下来-->
+      <router-view/>
+    </keep-alive>
+
   </div>
 </template>
 
@@ -12,12 +15,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import fastclick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 Vue.config.productionTip = false
-
+//解决移动端点击事件的300ms延迟
+fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
