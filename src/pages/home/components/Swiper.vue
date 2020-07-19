8fc36af41e3c9dab5197726cb20e127661de0a86
liliran <1341495943@qq.com>
2020-07-19 20:58:39 +0800

    home-swiper-commit

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg" alt="/">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg" alt="/2">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg" alt="/3">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+    import {swiper,swiperSlide} from 'vue-awesome-swiper'
+    export default {
+        name: "HomeSwiper",
+        components:{
+          swiperSlide,
+          swiper
+        },
+        data () {
+          return {
+            swiperOption: {
+              pagination: '.swiper-pagination',
+              autoplay: 2000,
+              loop: true
+            }
+          }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
