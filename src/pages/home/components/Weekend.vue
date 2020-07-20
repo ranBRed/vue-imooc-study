f898579e5b1825f8699ccc6a3a25a1951a279462
liliran <1341495943@qq.com>
2020-07-20 15:50:54 +0800

    home-weekend-commit

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li
+       class="item border-bottom">
+       <div class="item-img-wrapper">
+         <img class="item-img" src="http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg" alt="/"/>
+       </div>
+       <div class="item-info">
+         <p class="item-title">北京温泉排行榜</p>
+         <p class="item-desc">细数北京温泉，温暖你的冬天</p>
+       </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeWeekend"
+    }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.09%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+
+</style>
